5c86467a14b5b79dfb8dc85f8430aca50229a8a0
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-05-19 17:27:12 +0200

    changed head route

--- src/app/components/Root/Root.tsx
+++ src/app/components/Root/Root.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 export const Root = () => {
 	const router = createBrowserRouter([
 		{
-			path: "/cp-frontend-WoutVanImpe/",
+			path: "/cp-frontend-WoutVanImpe",
 			element: <App />,
 			children: [
 				{ path: HOME_ROUTE.path, element: HOME_ROUTE.element },
