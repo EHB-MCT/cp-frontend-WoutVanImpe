0783ad848c25fb8a92d1f06e91f894dd676faa85
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-05-20 23:37:29 +0200

    Revert "Add carousel component"

--- src/home/pages/Home.page.tsx
+++ src/home/pages/Home.page.tsx
@@ -3,7 +3,6 @@ import { SmallStoryCard } from "~home/components/smallStoryCard/SmallStoryCard";
 import { FairytaleType } from "~shared/hooks/fairytale.types";
 import { useGetFairytales } from "~shared/hooks/useGetFairytales.hooks";
 import { LoadingSpinner } from "~shared/components/loadingSpinner/LoadingSpinner";
-import { Carousel } from "~shared/components/carousel/Carousel";
 
 export const Home = () => {
 	const {
@@ -19,11 +18,10 @@ export const Home = () => {
 	) : (
 		<div className={styles["p-home"]}>
 			<h1>HOT TODAY</h1>
-			<Carousel data={fairytaleData} />
 			<h1>STORIES</h1>
 			<div className={styles["p-home__storyList"]}>
 				{fairytaleData?.map((fairytale: FairytaleType) => (
-					<SmallStoryCard key={`smallCard${fairytale.id}`} data={fairytale} />
+					<SmallStoryCard key={fairytale.id} data={fairytale} />
 				))}
 			</div>
 		</div>
