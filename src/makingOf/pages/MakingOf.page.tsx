fcd33320cb5175b2b5499658344c129cfbb951a4
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-05-21 00:14:10 +0200

    added default data to the making of page

--- src/makingOf/pages/MakingOf.page.tsx
+++ src/makingOf/pages/MakingOf.page.tsx
@@ -4,37 +4,36 @@ export const MakingOf = () => {
 	return (
 		<div className={styles["p-makingOf"]}>
 			<h1>MAKING OF</h1>
-			<div className={styles["p-makingOf__banner"]} style={{ backgroundImage: "url('http://i54.tinypic.com/4zuxif.jpg')" }}>
+			<div className={styles["p-makingOf__banner"]} style={{ backgroundImage: "url('./banner.png')" }}>
 				<div className={styles["p-makingOf__banner__text"]}>
-					<h2>Sprookje</h2>
-					<p>Student</p>
+					<h2>Blauwbaard</h2>
+					<p>Wout Van Impe</p>
 				</div>
 			</div>
 
 			<div className={styles["p-makingOf__info"]}>
-				<div>
-					<div>
-						<h5>Verhaal</h5>
-						<p>tekst</p>
-					</div>
-					<div>
-						<h5>Auteur</h5>
-						<p>tekst</p>
-					</div>
+				<div className={styles["p-makingOf__info__text"]}>
+					<h5>Verhaal</h5>
+					<p>
+						Blauwbaard is een duister sprookje vol mysterie en verboden geheimen. Het gaat over een rijk, maar zonderlinge edelman met een blauwe baard die al meerdere keren getrouwd is geweest, maar niemand weet wat er met zijn vorige vrouwen is
+						gebeurd. Wanneer hij opnieuw trouwt, geeft hij zijn jonge bruid de sleutels van zijn kasteel, met één strikte regel: één kamertje mag ze nooit betreden. De verleiding blijkt te groot, en achter die verboden deur ontdekt ze een
+						huiveringwekkend geheim… het lot van haar voorgangsters. Wanneer Blauwbaard haar daad ontdekt, dreigt ook zij zijn volgende slachtoffer te worden. Maar kan ze hem nog op tijd ontvluchten?{" "}
+					</p>
+					<h5>Auteur</h5>
+					<p>Charles Perrault</p>
+					<p>Horror</p>
 				</div>
 				<div>
-					<img src="" alt="sprookje" />
-					<button>Lees meer</button>
+					<img src="./extra1.png" alt="sprookje" />
 				</div>
 			</div>
 
-			<h1>EXTRA INFORMATIE</h1>
+			<h1>EXTRA AFBEELDINGEN</h1>
 			<div>
-				<img src="" alt="sprookje" />
-				<img src="" alt="sprookje" />
-				<img src="" alt="sprookje" />
+				<img src="./extra2.png" alt="sprookje" />
+				<img src="./extra3.png" alt="sprookje" />
+				<img src="./extra4.png" alt="sprookje" />
 			</div>
-			<p>tekst</p>
 		</div>
 	);
 };
