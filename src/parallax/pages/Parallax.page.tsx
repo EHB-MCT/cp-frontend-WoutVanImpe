d4dfd152f01d926157dc544a035d43e4c8510b82
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-05-26 14:04:41 +0200

    scene 5 done

--- src/parallax/pages/Parallax.page.tsx
+++ src/parallax/pages/Parallax.page.tsx
@@ -2,6 +2,7 @@ import Scene01 from "~parallax/scenes/Scene01";
 import Scene02 from "~parallax/scenes/Scene02";
 import Scene03 from "~parallax/scenes/Scene03";
 import Scene04 from "~parallax/scenes/Scene04";
+import Scene05 from "~parallax/scenes/Scene05";
 
 export const Parallax = () => {
 	return (
@@ -10,7 +11,7 @@ export const Parallax = () => {
 			<Scene02 />
 			<Scene03 />
 			<Scene04/>
-			<Scene02 />
+			<Scene05 />
 		</div>
 	);
 };
---
+++ src/parallax/scenes/Scene05.tsx
@@ -0,0 +1,55 @@
+import { useRef, useEffect } from "react";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import styles from "../pages/parallax.module.scss";
+
+const Scene05 = () => {
+	const scrollRef = useRef<HTMLDivElement>(null);
+
+	const { scrollYProgress } = useScroll({
+		target: scrollRef,
+		offset: ["start start", "end end"],
+	});
+
+	const smoothScroll = useSpring(scrollYProgress, {
+		stiffness: 50,
+		damping: 20,
+		mass: 1,
+	});
+
+	useEffect(() => {
+		scrollYProgress.onChange((v) => console.log("Scene05 progress", v));
+	}, [scrollYProgress]);
+
+	const womanScale = useTransform(smoothScroll, [0, 0.9], [1, 0.13]);
+    const womanOpacity = useTransform(smoothScroll, [0.1, 0.3], [0, 1]);
+	const womanY = useTransform(smoothScroll, [0, 0.9], [1000, 520]);
+	const womanX = useTransform(smoothScroll, [0, 0.9], ["-550%", "-80%"]);
+
+	const hallwayOpacity = useTransform(smoothScroll, [0, 0.1], [0, 1]);
+
+	return (
+		<div ref={scrollRef} className={`${styles.container} ${styles["container--scene05"]}`}>
+			{/* Sticky content */}
+			<div className={styles.stickyScene2}>
+				<motion.div className={styles.bgLayer} style={{ opacity: hallwayOpacity }}>
+					<img src="./scenes/scene05/room.png" alt="Gang met deur" className={styles.image} />
+				</motion.div>
+
+				<motion.div
+					className={styles.walkingWoman}
+					style={{
+						scale: womanScale,
+                        opacity: womanOpacity,
+						y: womanY,
+						x: womanX,
+						zIndex: 2,
+					}}
+				>
+					<img src="./scenes/scene05/woman.png" alt="Vrouw loopt naar deur" />
+				</motion.div>
+			</div>
+		</div>
+	);
+};
+
+export default Scene05;
